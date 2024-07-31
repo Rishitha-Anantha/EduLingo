f9ddaec402abee03e669600e3a212b3fab003f45
Rishitha Anantha <rishitha.anantha@gmail.com>
2024-07-31 11:53:38 +0100

    Modified to retrieve images from assets

--- App.js
+++ App.js
@@ -1,11 +1,11 @@
 import {View, Text, Image, StyleSheet } from 'react-native';
-const Img = require("./assets/image/4.jpg");
+const Img = require("./assets/image/2.jpg");
 
 export default function App(){
   return (
     <View style= {styles.container}>
       <Text>Welcome to Edulingo</Text>
-      <Image source={Img} style={{width: 300, height: 300}} />
+      <Image source={Img} />
     </View>
   );
 }
